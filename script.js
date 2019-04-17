b20d23edde8270ceed47d370b2e4ae4aa618c1ac
Laura Figueroa <lfigu042@fiu.edu>
2019-04-17 13:08:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ function assign_team(){
   
   //only continue function if player_name is valid
   if(team_name !== "" && isNaN(team_name)){
-  //Display team name on html
+    //Display team name on html
     var current_team = document.getElementById("playing-team");
     current_team.innerHTML = "Team " + team_name + "!!!";
+    show_player_form();
+    hide_team_form();
   }else{
     //say error, invalid input
     alert("team name must not be empty nor be a number");
@@ -26,7 +28,7 @@ function assign_name() {
     let name_char = player_name.charAt(0);
     let adj_arr = ["Adaptable", "Adventurous", "Affable", "Affectionate", "Agreeable", "Ambitious", "Amiable", "Amicable", "Amusing", "Brave", "Bright", "Broad-Minded", "Calm", "Careful", "Charming", "Communicative", "Compassionate", "", "Conscientious", "Considerate", "Convivial", "Courageous", "Courteous", "Creative", "Decisive", "Determined", "Diligent", "Diplomatic", "Discreet", "Dynamic", "Easygoing", "Emotional", "Energetic", "Enthusiastic", "Exuberant", "Fair-Minded", "Faithful", "Fearless", "Forceful", "Frank", "Friendly", "Funny", "Generous", "Gentle", "Good", "Gregarious", "Hard-Working", "Helpful", "Honest", "Humorous", "Imaginative", "Independent", "Intellectual", "Intelligent", "Intuitive", "Inventive","Jolly", "Joyfull","Jaunty", "Jovial","Jiggish", "Kind", "Keen", "Kick-ass","Knightly","Kosher","Loving", "Loyal", "Likeable", "Lighthearted", "Lively", "Loyal", "Lovely", "Lucky", "Modest", "Mature", "Martial", "Memorable","Merry","Meticulous","Mesmerized","Moral", "Neat", "Nice","Nourishing", "Optimistic", "Objective", "O.K.", "Open", "Opinionated", "Optimistic","Orderly","Outgoing","Outstanding","Overjoyed", "Passionate", "Patient", "Persistent", "Pioneering", "Philosophical", "Placid", "Plucky", "Polite", "Powerful", "Practical", "Pro-Active", "Quick-Witted","Quick", "Quiet", "Rational", "Reliable", "Reserved", "Resourceful", "Romantic", "Self-Confident", "Self-Disciplined", "Sensible", "Sensitive", "Shy", "Sincere", "Sociable", "Straightforward", "Sympathetic", "Thoughtful", "Tidy", "Tough","Unique","Unaffected","Unbiased", "Unassuming", "Understanding",  "Versatile", "Victorious", "Vivid", "Visionary", "Voracious", "Versatile", "Valiant", "Warmhearted","Wholesome","Wonderful","Winning","Wondrous", "Willing", "Witty","XOXOXOX", "Xenial","XisHardToWorkWith:(","Young","Youthful","Yummy", "Zealous", "Zestful"];
 
-    //scan adj_arr for adjectives that start with the same letter as the player_name and save them to adj_name
+  //scan adj_arr for adjectives that start with the same letter as the player_name and save them to adj_name
     let adj_name = [];
     for(let i = 0; i< adj_arr.length; i++){
       if(adj_arr[i].charAt(0) == name_char){
@@ -60,19 +62,28 @@ function assign_name() {
   }else{
     //say error, invalid input
     alert("player name must not be empty nor be a number");
-  } 
+  }
+  
+  
 }//END OF assign_name FUNCTION
 
 // GET SCORE
-// Press id="get-score" and obtain team name and team score and display under Highscores if score is more than the rest. 
-// Reset score and reset any names under "Remember these"
+// Press id="get-score" and obtain team name and team score and display under Highscores if score is more than the rest. Reset score and reset any names under "Remember these"
 function getScore(){
+  //add the team name and their score to the score board before they are deleted
   //find current score
   let score = get_count();
-  // console.log(score +" <--Current score");
+  //save the entered team name and their score
+  let team_name = document.getElementById("team-form").value.toUpperCase();
+  //add them to the array of teams and scores to be displayed onto scoreboard
+  add_teams(team_name, get_count());
   
   
   
+  //show the "enter team name" form
+  show_team_form();
+  //hide the "enter player name" form
+  hide_player_form();
   //reset current score to 0 and display it to HTML
   restart_count();
    //clear all names under "Remember these: "
@@ -123,17 +134,63 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//array with team names and their scores ex: "FIU":10
-var best_teams = [["FIU",5],["Dorks",6],["best", 10]];
-//add teams
+// display #player-name-input
+function show_player_form(){
+  document.getElementById("player-name-input").style.visibility = "visible";
+  document.getElementById("get-score").style.visibility = "visible";
+  document.getElementById("nicknames").style.visibility = "visible";
+}
+//set #player-name-input visibility: hidden
+function hide_player_form(){
+  document.getElementById("player-name-input").style.visibility = "hidden";
+  document.getElementById("get-score").style.visibility = "hidden";
+  document.getElementById("nicknames").style.visibility = "hidden";
+}
+// display #team-name-input and #get-score btn and #nicknames line
+function show_team_form(){
+  document.getElementById("team-name-input").style.visibility = "visible";
+}
+//hide #team-name-input and #get-score btn and #nicknames line 
+function hide_team_form(){
+  document.getElementById("team-name-input").style.visibility = "hidden";
+}
+
+//Displaying top three top scores
+// array with team names and their scores ex: "FIU":10
+var best_teams = [["a",0],["b",0],["c", 0]];
+var three_top;
+//add teams and sort them from highest score to lowest
 function add_teams(team,score){
   best_teams.push([team,score]);
+  best_teams.sort(function(a,b){
+    return b[1] - a[1];
+  });
+  //should post the three highest scores to the "highscore" board
   find_best_three(best_teams);
 }
-function find_best_three(teams){
-  let firstPlace;
-  let secondPlace;
-  let thirdPlace;
-  // console.log(teams);
+//the three top teams should be posted to the HTML "highscore" board
+function find_best_three(arr){
+  three_top = [[best_teams[0]],[best_teams[1]],[best_teams[2]]]
+  console.log(three_top);
+  //make first_place HTML element with id="gold-winner"
+  let first_place = best_teams[0].toString();
+  let first_place_name = first_place.split(",")[0];
+  let first_place_score = first_place.split(",")[1];
+  //make second_place HTML element with id="silver-winner"
+  let second_place = best_teams[1].toString();
+  let second_place_name = second_place.split(",")[0];
+  let second_place_score = second_place.split(",")[1];
+  //make third_place HTML element with id="bronze-winner"
+  let third_place = best_teams[2].toString();
+  let third_place_name = third_place.split(",")[0];
+  let third_place_score = third_place.split(",")[1];
+  
+  //if teams scored more than 0, display top three to score board
+  if(first_place_score > 0){
+    document.getElementById("gold-winner").innerHTML = first_place_name+"-"+first_place_score;
+  }if(second_place_score > 0){
+    document.getElementById("silver-winner").innerHTML = second_place_name+"-"+second_place_score;
+  }if(third_place_score > 0){
+    document.getElementById("bronze-winner").innerHTML = third_place_name+"-"+third_place_score;
+  }
 }
-add_teams("Nina", 80);
